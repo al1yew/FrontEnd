45bc93e5cf01db492863105d8983a5d1845aee4a
Vasif Aliyev <vasifja@code.edu.az>
2022-04-13 23:00:40 +0400

    Published changes

---
+++ assets/scripts/script.js
@@ -0,0 +1,14 @@
+let a = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  a++;
+  if (a > slides.length) {a = 1}
+  slides[a-1].style.display = "block";
+  setTimeout(showSlides, 2000);
+}
