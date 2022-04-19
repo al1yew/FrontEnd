44958dade52b601f1411ba2c388abdc42ed1d6f3
Vasif Aliyev <vasifja@code.edu.az>
2022-04-19 10:11:33 +0400

    Published changes

--- April_18_HW_2 Counter/assets/js/script.js
+++ April_18_HW_2 Counter/assets/js/script.js
@@ -9,10 +9,31 @@ function Increase() {
 
 function Decrease() {
 
+    // let result = Number(document.getElementById('result').innerHTML);
+    // result--;
+    // document.getElementById('result').innerHTML = result;
+
+    //bu reset-i 0 olmaq uchundu, hemde menfi regemlere gede bilir.
+
+    
     let result = Number(document.getElementById('result').innerHTML);
-    result--;
-    document.getElementById('result').innerHTML = result;
+    if(result>1){
+        result--;
+        document.getElementById('result').innerHTML = result;
+    }
+    else{
+        document.getElementById('result').innerHTML = 1;
+    }
+    
     
 }
 
 let Reset = () => document.getElementById('result').innerHTML = 0;
+
+
+
+
+function Decrease() {
+
+
+}
