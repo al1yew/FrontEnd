9cdc4f046e26006aaebc0f57cbe70e4144cf072b
Vasif Aliyev <vasifja@code.edu.az>
2022-04-20 16:35:23 +0400

    Published changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,15 +26,30 @@ input.onblur = () => {
 
 btn.onclick = () => {
 
-    let input = document.getElementById('input'); //our input
+    let input_value = document.getElementById('input').value; //our input
     let list = document.getElementById('list'); //out list ul
     let li = document.createElement('li'); // list elements li
     let span = document.createElement('span'); // span in li (add method)
     let del = document.createElement('button'); // delete button
     let edit = document.createElement('button'); // edit button
+    let i_del = document.createElement('i');
+    let i_edit = document.createElement('i');
 
+    span.innerHTML = input_value;
+
+
+
+
+
+    i_edit.className = 'fa-solid fa-pen';
+    i_del.className = 'fa-solid fa-eraser';
     
-
+    list.appendChild(li);
+    li.appendChild(span);
+    li.appendChild(del);
+    li.appendChild(edit);
+    del.appendChild(i_del);
+    edit.appendChild(i_edit);
 
 
 
