7aa2a1643585e6025a6b7c97d69ab9eaf7a55485
Vasif Aliyev <vasifja@code.edu.az>
2022-04-21 09:18:57 +0400

    Published changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,6 @@ input.onblur = () => {
 // dynamic list creation
 
 btn.onclick = () => {
-
     let input = document.getElementById('input');
     let input_value = document.getElementById('input').value;
     let list = document.getElementById('list');
@@ -36,8 +35,8 @@ btn.onclick = () => {
     
     // if (input_value.lenght > 0) {
 
-    //     let del_id = document.getElementsByClassName('del');
-    //     let edit_id = document.getElementsByClassName('edit');
+    //     let del_class = document.getElementsByClassName('del');
+    //     let edit_class = document.getElementsByClassName('edit');
 
     //     list.appendChild(li);
     //     li.appendChild(span);
@@ -54,38 +53,25 @@ btn.onclick = () => {
     del.className = 'fa-solid fa-eraser del';
     edit.className = 'fa-solid fa-pen edit';
     
-    let edit_id = document.getElementsByClassName('edit');
-    let del_id = document.getElementsByClassName('del');
+    let edit_class = document.getElementsByClassName('edit');
+    let del_class = document.getElementsByClassName('del');
 
     list.appendChild(li);
     li.appendChild(span);
     li.appendChild(edit);
     li.appendChild(del);
 
-    for (let i of del_id) {
+    for (let i of del_class) {
         i.onclick = function (e) {
             e.target.parentElement.remove();
         }
     }
 
-    for (let z of edit_id) {
-        z.onclick = function (k) {
-            // li.firstElementChild.span.innerHTML = '';
-            // k.target.parentElement.span.innerHTML = '';
-            // k.target.parentElement.span.innerHTML = 'asdasd'
-            // k.target.parentElement.replace('asd','dede');
-            k.target
+    for (let btn of edit_class) {
+        btn.onclick = () => {
+            
         }
     }
 
 
-
-
-    document.getElementById('input').value = '';
-
-    //buttonu basanda input 0 olsun
-
 }
-
-//o funkcii
-
