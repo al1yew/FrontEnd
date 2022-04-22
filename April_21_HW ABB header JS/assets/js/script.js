9bef944d25a6fc83a591b3d27e28683c6164a51a
Vasif Aliyev <vasifja@code.edu.az>
2022-04-22 17:51:23 +0400

    Published changes

--- April_21_HW ABB header JS/assets/js/script.js
+++ April_21_HW ABB header JS/assets/js/script.js
@@ -7,7 +7,7 @@ let search = document.getElementById('search_engine');
 
 let quit = document.getElementById('quit');
 
-quit.onclick = function() {
+quit.onclick = function () {
     search.classList.add('for_visibility')
 }
 
@@ -16,10 +16,54 @@ arrow.onclick = function () {
     if (search.classList.contains('for_visibility')) {
         search.classList.remove('for_visibility')
     }
-    else{
+    else {
         search.classList.add('for_visibility')
     }
 }
 //#endregion
+
+
+let langs = document.getElementById('langs_shadow');
+
+let lang_btn = document.getElementById('lang')
+
+
+lang_btn.onclick = function () {
+
+    if (langs.classList.contains('for_visibility1')) {
+        langs.classList.remove('for_visibility1')
+    }
+    else {
+        langs.classList.add('for_visibility1')
+    }
+}
+
+
+
+
+
+let btn = document.getElementById('tap_it');
+
+let languages = document.getElementById('hidden_langs');
+
+btn.onclick = function() {
+
+    if (languages.classList.contains('hide_show')) {
+        languages.classList.remove('hide_show')
+    }
+    else {
+        languages.classList.add('hide_show')
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 
