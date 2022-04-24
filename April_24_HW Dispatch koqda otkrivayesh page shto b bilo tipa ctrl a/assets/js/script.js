c011af911e0a1fe28cae857f58d5285ee7989c6f
Vasif Aliyev <vasifja@code.edu.az>
2022-04-24 14:55:17 +0400

    Published changes

---
+++ April_24_HW Dispatch koqda otkrivayesh page shto b bilo tipa ctrl a/assets/js/script.js
@@ -0,0 +1,33 @@
+
+
+let p = document.getElementById('p');
+
+// for(i=0; i<3; i++){
+//     alert('asd')
+//     window.location.reload();
+// }
+
+// p.focus()
+
+// window.focus();
+
+
+// console.log(window.getSelection(getElementById('p')))
+
+// window.getSelection().toString();
+
+// console.log(window.getSelection().toString())
+
+// let ctrlEvent = new KeyboardEvent("keydown", {bubbles : true, cancelable : true, keyCode : 17, char : 17, shiftKey : true});
+// let aEvent = new KeyboardEvent("keydown", {bubbles : true, cancelable : true, keyCode : 65, char : 65, shiftKey : true});
+
+// document.dispatchEvent(ctrlEvent);
+// document.dispatchEvent(aEvent);
+
+// function highlight(text) {
+//     let p = document.getElementById("p");
+//     
+// }
+
+
+// nese bu alinmadi
