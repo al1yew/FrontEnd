78e28dd089e29e6421c84c32b3c52755ac470d60
Vasif Aliyev <vasifja@code.edu.az>
2022-04-24 16:18:40 +0400

    Published changes

---
+++ assets/scripts/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.logo-carousel').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        pauseOnHover: false,
+    });
+});
