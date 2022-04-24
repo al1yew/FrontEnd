546361c8b01c6ac79474fc1b5b85b52ee847a609
Vasif Aliyev <vasifja@code.edu.az>
2022-04-24 23:15:44 +0400

    Published changes

---
+++ assets/scripts/script.js
@@ -0,0 +1,29 @@
+
+let slideNo = 0;
+
+let slide = document.getElementsByClassName("slide_hide");
+let dot = document.getElementsByClassName("dot");
+
+Slider();
+
+function Slider() {
+    
+    for(let _slide of slide){
+        _slide.style.display = 'none';
+    }
+
+    slideNo++;
+
+    if (slideNo > slide.length) {
+        slideNo = 1
+    }
+
+    for (i = 0; i < dot.length; i++) {
+        dot[i].className = dot[i].className.replace(" active", "");
+    }
+
+    slide[slideNo - 1].style.display = "flex";
+    dot[slideNo - 1].className += " active";
+
+    setTimeout(Slider, 3000);
+}
