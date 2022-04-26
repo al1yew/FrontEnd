15ae61a5c50739de7bb4c4e15868b9e11e0ea3f9
Vasif Aliyev <vasifja@code.edu.az>
2022-04-27 01:09:46 +0400

    Published changes

--- File reader/assets/js/script.js
+++ File reader/assets/js/script.js
@@ -81,14 +81,15 @@ download.nextElementSibling.onchange = function (e) { //download icondu deye ve 
       //demeli fayl pdf-dise if-in shertleri yerine getirilir. 
 
       //#endregion
-      // let spans = document.querySelectorAll('#remove');
-      // for(let span of spans){
-      //   span.onclick = function() {
-      //     e.target.parentElement.remove();
-      //   }
-      // }
 
-      //                                                                       BURDA MARAGLI SHEYLER VAR DUZELT
+      let spans = document.querySelectorAll('#remove');
+      for(let span of spans){
+        span.onclick = function(m) {
+          m.target.parentElement.parentElement.remove();
+        }
+      }
+
+      //                    bunu nece otraya getirim bosh yerine p atmisham ki :/          BURDA MARAGLI SHEYLER VAR DUZELT
       //#region Remove
 
 
