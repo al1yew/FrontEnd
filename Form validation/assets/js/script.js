99221e37a9956121e462c4dd4edbfcb658d2b2c3
Vasif Aliyev <vasifja@code.edu.az>
2022-04-27 12:46:25 +0400

    Published changes

---
+++ Form validation/assets/js/script.js
@@ -0,0 +1,18 @@
+let email = document.getElementById('email');
+
+let email_regex = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)
+
+let btn = document.getElementById('submit');
+
+addEventListener('click', function () {
+
+    // console.log(email_regex.test(email.value));
+
+    if(email_regex.test(email.value)){
+        email.style.boxShadow = '0px 0px 5px 2px blue'
+    }
+    else{
+        this.alert('Write eMail in right way!')
+    }
+
+})
