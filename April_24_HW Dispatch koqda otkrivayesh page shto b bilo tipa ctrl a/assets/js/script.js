0e07e571ee0ffc5ac20e48236f8b4d512ee5d3f0
Vasif Aliyev <vasifja@code.edu.az>
2022-04-28 10:34:52 +0400

    Published changes

--- April_24_HW Dispatch koqda otkrivayesh page shto b bilo tipa ctrl a/assets/js/script.js
+++ April_24_HW Dispatch koqda otkrivayesh page shto b bilo tipa ctrl a/assets/js/script.js
@@ -1,33 +1,17 @@
+function Start() {
+    window.addEventListener('load', ()=>{
+		document.dispatchEvent(new KeyboardEvent('keypress', {'key': 'ctrlKey'}));
+		document.dispatchEvent(new KeyboardEvent('keypress', {'key': 'shiftKey'}));
+		document.dispatchEvent(new KeyboardEvent('keypress', {'key': '6'}));
+});
+    document.addEventListener('keypress', (event)=>{
+        document.body.style.backgroundColor = 'red';
+    });
+}
 
 
-let p = document.getElementById('p');
 
-// for(i=0; i<3; i++){
-//     alert('asd')
-//     window.location.reload();
-// }
+let input = document.getElementsByName('Captcha')[0];
 
-// p.focus()
-
-// window.focus();
-
-
-// console.log(window.getSelection(getElementById('p')))
-
-// window.getSelection().toString();
-
-// console.log(window.getSelection().toString())
-
-// let ctrlEvent = new KeyboardEvent("keydown", {bubbles : true, cancelable : true, keyCode : 17, char : 17, shiftKey : true});
-// let aEvent = new KeyboardEvent("keydown", {bubbles : true, cancelable : true, keyCode : 65, char : 65, shiftKey : true});
-
-// document.dispatchEvent(ctrlEvent);
-// document.dispatchEvent(aEvent);
-
-// function highlight(text) {
-//     let p = document.getElementById("p");
-//     
-// }
-
-
-// nese bu alinmadi
+$(input).focus();
+Start();
