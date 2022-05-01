e2c6dc72276857a1d1389bfa423cd13bf4603033
Vasif Aliyev <vasifja@code.edu.az>
2022-05-01 17:42:34 +0400

    Published changes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -28,8 +28,49 @@
 //   setTimeout(Slider, 3000);
 // }
 
+
+$('.slider1').slick({
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 2000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: false,
+        arrows: false
+      }
+    }
+  ]
+});
+
 $('.slider').slick({
-  arrows: true,
+  arrows: false,
   dots: true,
   infinite: true,
   speed: 2000,
