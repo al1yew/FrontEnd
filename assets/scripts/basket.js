2bb0ef2abd49f17bc0b0f2f5f53c80ee4e97d3f7
Vasif Aliyev <vasifja@code.edu.az>
2022-05-12 00:14:20 +0400

    Published changes

--- assets/scripts/basket.js
+++ assets/scripts/basket.js
@@ -79,3 +79,28 @@ CountBasketLength();
 //#endregion shopping cartin ustundeki span
 
 
+
+//#region yoxlayirig basket boshdu ya yox
+
+let shopcart = JSON.parse(localStorage.getItem('shopcart'));
+
+//basketimizi cagirdig, parse eledik
+
+if (shopcart.length === 0) {
+    //nado udalit tot divk otoriy ya sozdam v cert html, esli basket ne soderjit nikakix tovarov. ya emu inline dam d-none, i poka basket length raven 0, on budet u mena display none, a kak tolko basket length budet ne 0, on classlist remove sdelayet d-none
+}
+else{
+    //nado add classslist sdelat d-none
+    //takje tut nado sozdat ogromniy designed div, v kotoriy kak v to do list v kotoriy budut dobavlatsa vse nashi tovari. dla etogo mi nash basket zasunem v for, i dla kajdogo elementa budem sozdavat div, i vnutr p, span, h6 zad, budem delat set hemin let basket_item naprimer of basket-in . name, .price, .count itd eto krc rekord nomer 2 pod konec posle 38 minuti
+    //console logom proveru prineset li on mne kak list moi tovari v console
+    //uchuncu rekord 10cu deqiqeden, mene oz counterim lazimdi burda maragli olsun
+    //refreshe bax kodda ve clear all a bax
+    //delete iconlari nece edirik, butun delete iconlari saliram fora, deyirem ki hansina klik olunsa, funksiya bash versin. Saliram basketi fora, dyirem ki basketin icindeki butun elementlerin icinde axtar. Neyi? delete buttona click olunan elementin id-sinin basketin icinde olanini, ve onu sil ordan.
+}
+
+
+
+
+//#endregion yoxlayirig basket boshdu ya yox
+
+
