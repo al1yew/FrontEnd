9c943f2574d34195042ca12b06c7f884cb0b6735
Vasif Aliyev <vasifja@code.edu.az>
2022-05-14 15:00:09 +0400

    Published changes

--- assets/scripts/basket.js
+++ assets/scripts/basket.js
@@ -20,14 +20,12 @@ for (let btntoadd of addtocartbtn) {
 
         let prod_id = this.parentElement.parentElement.parentElement.parentElement.id
         // console.log(prod_id)
-        let prod_img_src = this.parentElement.parentElement.parentElement.children[0].src;
+        let prod_img_src = this.parentElement.parentElement.parentElement.children[0].children[0].src;
 
         let prod_name = this.parentElement.parentElement.children[0].innerHTML
 
         let prod_cost = this.parentElement.previousElementSibling.children[2].innerHTML;
         //butun melumatlari goturdum
-
-        // amma diyesen hec lazim olmadi // let prod_count = 1; //bunu mecbur verdik cunki objectin icindeki Count-a not defined deyir, onu burda yaradirig ve 1 veririk ki, produktu elave edende onsuzda 1 den bashlayir
 
         let prodexists = shopcart.find(x => x.Id == prod_id);
 
@@ -86,16 +84,116 @@ CountBasketLength();
 let shopcart = JSON.parse(localStorage.getItem('shopcart'));
 let empty_basket = document.querySelector('#contentbasket');
 let filled_basket = document.querySelector('#prod_in_cart');
+let little_filled_basket = document.querySelector('#polniy_basket')
+let little_empty_basket = document.querySelector('#pustoy_basket')
 
 //basketimizi cagirdig, parse eledik
 // bosh olanda yaradilan sectionu basket boshdursa gostereceyik
 
-if (shopcart.length === 0) {
+if (shopcart.length == 0) {
     empty_basket.classList.remove('d-none');
     filled_basket.classList.add('d-none');
-    
+    little_empty_basket.classList.remove('d-none');
+    little_filled_basket.classList.add('d-none');
+    // console.log(empty_basket)
+    // console.log(filled_basket)
+    // console.log(little_empty_basket)
+    // console.log(little_filled_basket)
 }
 else {
+
+    empty_basket.classList.add('d-none');
+    filled_basket.classList.remove('d-none');
+    little_empty_basket.classList.add('d-none');
+    little_filled_basket.classList.remove('d-none');
+
+    let pr_big = '';
+    let pr_small = '';
+    let small_basket_inner = document.querySelector('#small_basket_innner');
+    let big_basket_inner_tbody = document.querySelector('#tbody_basket');
+
+    for (let product of shopcart) {
+        pr_big +=
+            `
+            <tr>
+                <td class="col-lg-1">
+                    <img class="img-fluid" src="${product.Src}">
+                </td>
+                <td class="col-lg-6">
+                    ${product.Name}
+                </td>
+                <td class="col-lg-1">
+                    ${product.Price}$
+                </td>
+                <td class="col-lg-2">
+                    <div class="top_counter col-lg-12">
+                        <button id="decrease">
+                            -
+                        </button>
+
+                        <span id="result_count">
+                            ${product.Count}
+                        </span>
+
+                        <button id="increase">
+                            +
+                        </button>
+                    </div>
+
+                    <div class="bottom_counter">
+                        <span id="max10">You can add max 10</span>
+                    </div>
+                </td>
+                <td class="col-lg-1">
+                    ${Math.round((product.Price * product.Count) * 100) / 100}$
+                </td>
+                <td id="remove_prod_basket" class="col-lg-0-5">
+                    X
+                </td>
+            </tr>
+        `
+
+        pr_small +=
+            `
+            <div class="product_bag_div">
+
+                <div class="left col-lg-3">
+                    <img src="${product.Src}">
+                    <span id="remove_prod_basket" class="remove_x_small_basket">
+                        x
+                    </span>
+                </div>
+
+                <div class="right col-lg-9">
+
+                    <div class="top">
+                        ${product.Name}
+                    </div>
+
+                    <div class="bottom">
+                        <span>${product.Count} pcs.</span>
+                        <span>x</span>
+                        <span>${product.Price}$</span>
+                    </div>
+
+                </div>
+
+            </div>
+        `
+        small_basket_inner.innerHTML = pr_small;
+        big_basket_inner_tbody.innerHTML = pr_big;
+
+    }
+    // console.log(empty_basket)
+    // console.log(filled_basket)
+    // console.log(little_empty_basket)
+    // console.log(little_filled_basket)
+
+
+
+
+
+
     //nado add classslist sdelat d-none
     //takje tut nado sozdat ogromniy designed div, v kotoriy kak v to do list v kotoriy budut dobavlatsa vse nashi tovari. dla etogo mi nash basket zasunem v for, i dla kajdogo elementa budem sozdavat div, i vnutr p, span, h6 zad, budem delat set hemin let basket_item naprimer of basket-in . name, .price, .count itd eto krc rekord nomer 2 pod konec posle 38 minuti
     //console logom proveru prineset li on mne kak list moi tovari v console
